ba800495d88f1c69c8a9f54ba7a01bfaedd19015
noor153 <noor153@gmail.com>
2022-06-19 03:41:54 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,50 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
+import { TokenSaleComponent } from './token-sale/token-sale.component';
+import { DocsComponent } from './docs/docs.component';
+import { RoadMapComponent } from './road-map/road-map.component';
+import { GroupComponent } from './group/group.component';
+import { DesktopComponent } from './desktop/desktop.component';
+import { TeamComponent } from './team/team.component';
+import { PartnersComponent } from './partners/partners.component';
+import { FooterComponent } from './footer/footer.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
+import { FaqsComponent } from './faqs/faqs.component';
+import { ContactComponent } from './contact/contact.component';
+
+// import { FontAwesomeModule  } from '@fortawesome/angular-fontawesome/fontawesome.module';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    SecondComponent,
+    ThirdComponent,
+    TokenSaleComponent,
+    DocsComponent,
+    RoadMapComponent,
+    GroupComponent,
+    DesktopComponent,
+    TeamComponent,
+    PartnersComponent,
+    FooterComponent,
+    SubscribeComponent,
+    FaqsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/token-sale/token-sale.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+// import { faVcard } from '@fortawesome/free-solid-svg-icons'
+@Component({
+  selector: 'app-token-sale',
+  templateUrl: './token-sale.component.html',
+  styleUrls: ['./token-sale.component.css']
+})
+export class TokenSaleComponent {
+  // visa = faVcard
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
