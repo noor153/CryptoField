a57a6c5793e48dcf13c36fec0ff392fd381a8296
noor153 <noor153@gmail.com>
2022-06-20 05:28:59 +0200

    Responsive Update All

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/crypto-field'));
-app.get('/*', function(req,res){res.sendFile(path.join(__dirname+'/dist/crypto-field/index.html'));});
+app.use(express.static(__dirname + './crypto-field'));
+app.get('/*', function(req,res){res.sendFile(path.join(__dirname+'./crypto-field/index.html'));});
 app.listen(process.env.PORT || 8080);
